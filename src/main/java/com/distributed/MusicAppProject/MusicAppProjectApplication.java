3077bc466de1866b04caca373ed37b792dd2deff
melihacil <melihacil20@gmail.com>
2023-12-12 16:12:58 +0300

    First initialization

---
+++ src/main/java/com/distributed/MusicAppProject/MusicAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.distributed.MusicAppProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/distributed/MusicAppProject/MusicAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed.MusicAppProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
