e583c678844d14012d992e4fc9c50b581e842f9c
melihacil <melihacil20@gmail.com>
2024-01-06 14:30:35 +0300

    Updating bootRun stuff

--- src/main/java/com/distributed/MusicAppProject/MusicAppProjectApplication.java
+++ src/main/java/com/distributed/MusicAppProject/MusicAppProjectApplication.java
@@ -1,14 +1,33 @@
 package com.distributed.MusicAppProject;
 
+import com.distributed.MusicAppProject.Services.APIServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class })
 public class MusicAppProjectApplication {
 
+	public static String deezerCode;
 	public static void main(String[] args) {
+
 		SpringApplication.run(MusicAppProjectApplication.class, args);
+		try {
+			System.out.println("https://connect.deezer.com/oauth/auth.php?app_id=657351&redirect_uri=https://localhost/callback&perms=basic_access");
+			System.out.print("Please enter code from deezer: ");
+
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(System.in));
+			deezerCode = reader.readLine();
+			APIServiceImpl.DeezerApiInit(deezerCode);
+		}
+		catch (Exception e) {
+			System.out.println("Error Getting Deezer Code!");
+			e.printStackTrace();
+		}
 	}
 
 //	@Bean
--- src/main/java/com/distributed/MusicAppProject/Services/APIServiceImpl.java
+++ src/main/java/com/distributed/MusicAppProject/Services/APIServiceImpl.java
@@ -2,8 +2,12 @@ package com.distributed.MusicAppProject.Services;
 
 import api.deezer.DeezerApi;
 import api.deezer.exceptions.DeezerException;
-import api.deezer.objects.*;
+import api.deezer.objects.Album;
+import api.deezer.objects.DeezerAccessToken;
+import api.deezer.objects.Track;
+import api.deezer.objects.User;
 import api.deezer.objects.data.TrackData;
+import com.distributed.MusicAppProject.MusicAppProjectApplication;
 import com.distributed.MusicAppProject.Services.Interfaces.APIService;
 import com.distributed.MusicAppProject.Utils.HTTPConnection;
 import com.distributed.MusicAppProject.Utils.SIMHRestTemplate;
@@ -11,21 +15,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Scanner;
-
 @Service
 @RequiredArgsConstructor
 public class APIServiceImpl implements APIService {
 
-    private SIMHRestTemplate restTemplate;
 
-    DeezerApi deezerApi = new DeezerApi();
+    static DeezerApi deezerApi; //= new DeezerApi();
     /**
      * Instantiates a new API service.
      *
-     * @param httpConnectionUtils the http connection utils
+     * httpConnectionUtils the http connection utils
      */
-
+    private SIMHRestTemplate restTemplate;
     private static final int APP_ID = 657351;
 
     /**
@@ -42,27 +43,46 @@ public class APIServiceImpl implements APIService {
     @Autowired
     public APIServiceImpl(HTTPConnection httpConnectionUtils){
         this.restTemplate = new SIMHRestTemplate(httpConnectionUtils);
-
         this.deezerApi = new DeezerApi();
         try {
-            String loginUrl = deezerApi.auth().getLoginUrl(APP_ID, REDIRECT_URI, Permission.BASIC_ACCESS);
-            System.out.println(loginUrl); // https://connect.deezer.com/oauth/auth.php?app_id=123&redirect_uri=your.domain.com&perms=basic_access
-            System.out.println("https://connect.deezer.com/oauth/auth.php?app_id=657351&redirect_uri=https://localhost/callback&perms=basic_access");
-            System.out.print("Please enter code: ");
-            String code = new Scanner(System.in).next();
+            //Scanner scanner = new Scanner(System.in);
+            //String loginUrl = deezerApi.auth().getLoginUrl(APP_ID, REDIRECT_URI, Permission.BASIC_ACCESS);
+            //System.out.println(loginUrl); // https://connect.deezer.com/oauth/auth.php?app_id=123&redirect_uri=your.domain.com&perms=basic_access
+            //System.out.println("https://connect.deezer.com/oauth/auth.php?app_id=657351&redirect_uri=https://localhost/callback&perms=basic_access");
+            //System.out.print("Please enter code from deezer: ");
 
             // Step 3. Get access_token.
-            DeezerAccessToken accessToken = deezerApi.auth().getAccessToken(APP_ID, SECRET, code).execute();
-            deezerApi.setAccessToken(accessToken);
 
-            // Now we are ready to execute any request we want.
-            distProject = deezerApi.user().getMe().execute();
-            System.out.println(distProject);
 
         }
-        catch (DeezerException e) {
+        catch (Exception e) {
+            System.out.println("DEEZER ERROR ERROR!");
             e.printStackTrace();
         }
+    }
+
+    public static void DeezerApiInit(String deezerCode) {
+
+        deezerApi = new DeezerApi();
+        try {
+            //Scanner scanner = new Scanner(System.in);
+            //String loginUrl = deezerApi.auth().getLoginUrl(APP_ID, REDIRECT_URI, Permission.BASIC_ACCESS);
+            //System.out.println(loginUrl); // https://connect.deezer.com/oauth/auth.php?app_id=123&redirect_uri=your.domain.com&perms=basic_access
+            //System.out.println("https://connect.deezer.com/oauth/auth.php?app_id=657351&redirect_uri=https://localhost/callback&perms=basic_access");
+            //System.out.print("Please enter code from deezer: ");
+
+            // Step 3. Get access_token.
+            DeezerAccessToken accessToken = deezerApi.auth().getAccessToken(APP_ID, SECRET, MusicAppProjectApplication.deezerCode).execute();
+            deezerApi.setAccessToken(accessToken);
+
+        }
+        catch (Exception e) {
+            System.out.println("DEEZER ERROR ERROR!");
+            e.printStackTrace();
+        }
+        // Now we are ready to execute any request we want.
+        //        distProject = deezerApi.user().getMe().execute();
+        //        System.out.println(distProject);
     }
 
     @Override
